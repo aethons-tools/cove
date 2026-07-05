94ee60dbddc7cb12993c9b72c69ac27b2f92a887
Brent <brent@lcbm.life>
2026-07-05 23:29:26 +0000

    refactor: move at-cove entry into cmd/at-cove/

--- scripts/build.sh
+++ scripts/build.sh
@@ -45,7 +45,7 @@ for t in "${TARGETS[@]}"; do
   mkdir -p "$dir"
   echo "  building ${t} -> ${dir}/at-cove"
   CGO_ENABLED=0 GOOS="$os" GOARCH="$arch" \
-    go build -trimpath -ldflags "$LDFLAGS" -o "$dir/at-cove" .
+    go build -trimpath -ldflags "$LDFLAGS" -o "$dir/at-cove" ./cmd/at-cove
   built+=("$dir/at-cove")
 done
 
